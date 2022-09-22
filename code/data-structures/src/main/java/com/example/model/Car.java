a1482874202867cfb3df52d4013ebfc1f473f5e4
Nagabhushanam <nagcloudlab@gmail.com>
2022-09-22 17:15:14 +0530

    collections ( set & map )

---
+++ code/data-structures/src/main/java/com/example/model/Car.java
@@ -0,0 +1,20 @@
+package com.example.model;
+
+public class Car {
+
+    private String model;
+    private String color;
+
+    public Car(String model, String color) {
+        this.model = model;
+        this.color = color;
+    }
+
+    @Override
+    public String toString() {
+        return "Car{" +
+                "model='" + model + '\'' +
+                ", color='" + color + '\'' +
+                '}';
+    }
+}
