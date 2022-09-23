0b02ff0d34bfd7bd52669ca7b54c33347982f226
Nagabhushanam <nagcloudlab@gmail.com>
2022-09-23 16:28:21 +0530

    Ex

---
+++ code/streams-api/src/main/java/com/example/Ex1.java
@@ -0,0 +1,18 @@
+package com.example;
+
+import com.example.model.Dish;
+
+import java.util.List;
+
+public class Ex1 {
+
+    public static void main(String[] args) {
+
+
+        List<Dish> menu= Dish.menu;
+        // get low-caloric ( < 400 ) dish names ( sorted caloric values )
+
+
+    }
+
+}
