88563e8c33a5d58cb243aca6db09e0913a65e665
Nagabhushanam <nagcloudlab@gmail.com>
2022-09-23 17:56:26 +0530

    MR Q

--- code/functional-programming/src/main/java/com/example/MethodReference.java
+++ code/functional-programming/src/main/java/com/example/MethodReference.java
@@ -1,7 +1,16 @@
 package com.example;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.function.LongSupplier;
 import java.util.function.Supplier;
+
+//
+class FoodUtil{
+    public static boolean isNonVeg(String item){
+        return item.equals("nveg");
+    }
+}
 
 public class MethodReference {
     public static void main(String[] args) {
@@ -15,13 +24,30 @@ public class MethodReference {
 //
         // - or
 
-        LongSupplier longSupplier=System::currentTimeMillis;
-
-        long millis=longSupplier.getAsLong();
-        System.out.println(millis);
+//        LongSupplier longSupplier=System::currentTimeMillis;
+//
+//        long millis=longSupplier.getAsLong();
+//        System.out.println(millis);
 
 
         //-----------------------------------------------------------
+
+        List<String> menu=new ArrayList<>();
+        menu.add("veg");
+        menu.add("nveg");
+        menu.add("nveg");
+        menu.add("veg");
+        menu.add("nveg");
+
+
+        //
+//        menu.removeIf(item->item.equals("nveg"));
+//        menu.removeIf(item->FoodUtil.isNonVeg(item));
+        menu.removeIf(FoodUtil::isNonVeg);
+        System.out.println(menu);
+
+        //-----------------------------------------------------------
+
 
     }
 }
