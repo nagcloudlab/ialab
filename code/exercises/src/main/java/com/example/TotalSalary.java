ba5ddd4f883feb99a92b4be4a282d7cce0b51ec5
Nagabhushanam <nagcloudlab@gmail.com>
2022-09-28 14:34:58 +0530

    Ex2

---
+++ code/exercises/src/main/java/com/example/TotalSalary.java
@@ -0,0 +1,20 @@
+package com.example;
+
+public class TotalSalary {
+    public static void main(String[] args) {
+
+        String[] report = {
+                "E1,IT,1000",
+                "E2,HR,2000",
+                "E3,IT,3000",
+                "E4,IT,1000",
+                "E5,SALES,2000"
+        };
+
+        // find total salary of each department.
+        // condition: we don't how many departments exist in report
+
+
+
+    }
+}
