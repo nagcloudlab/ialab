d1edab27a626d84a80ecf917312c2a825d960661
nag <nagcloudlab@gmail.com>
2022-10-26 16:10:43 +0530

    JPA concepts

--- code/JPA-intro/src/main/java/com/example/Create.java
+++ code/JPA-intro/src/main/java/com/example/Create.java
@@ -1,6 +1,5 @@
 package com.example;
 
-import com.example.entity.Address;
 import com.example.entity.Department;
 import com.example.entity.Employee;
 import com.example.entity.Gender;
@@ -8,21 +7,24 @@ import com.example.entity.Gender;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
-import java.util.Date;
-import java.util.List;
 
-public class JpaIntro {
+public class Create {
     public static void main(String[] args) {
 
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("myPU");
         EntityManager em = emf.createEntityManager();
         em.getTransaction().begin();
 
+        Employee employee = new Employee();   // New / Transient
+        employee.setName("Pov");
+        employee.setGender(Gender.MALE);
+        Department department = new Department();
+        department.setId(3);
+        employee.setDepartment(department);
 
-        Department department = em.find(Department.class,3);
-        System.out.println(department.getName());
-        System.out.println("-".repeat(10));
-        department.getEmployees().forEach(e -> System.out.println(e.getName()));
+        em.persist(employee);  // Managed / Persistence State
+
+        employee.setGender(Gender.MALE);
 
         em.getTransaction().commit();
         em.close();
