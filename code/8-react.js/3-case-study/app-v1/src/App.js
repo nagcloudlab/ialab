24b9a3d43fc8bc6859190ddf4d018cafee348a10
nag <nagcloudlab@gmail.com>
2022-12-12 14:05:01 +0530

    Ex, fix the bux

--- code/8-react.js/3-case-study/app-v1/src/App.js
+++ code/8-react.js/3-case-study/app-v1/src/App.js
@@ -1,7 +1,11 @@
-
+import React, { useState } from 'react'
+import classNames from 'classnames';
 
 
 function App() {
+
+  const [tab, setTab] = useState(1);
+
 
   const products = [
     {
@@ -22,6 +26,14 @@ function App() {
     }
   ]
 
+  const renderTabPane = (product) => {
+    switch (tab) {
+      case 1: return (<div>{product.description}</div>)
+      case 2: return (<div>{"Not Yet"}</div>)
+      case 3: return (<div>{"None Yet"}</div>)
+    }
+  }
+
   const renderProduct = product => {
     if (product.isAvailable) {
       return (
@@ -33,8 +45,21 @@ function App() {
             <div className="col-sm-8 col-md-8">
               <div>{product.name}</div>
               <div>&#8377;{product.price}</div>
-              <div>{product.description}</div>
               <button className="btn btn-sm btn-dark">Buy</button>
+              <br /><br />
+              <ul className="nav nav-tabs">
+                <li className="nav-item">
+                  <a className={classNames({ 'nav-link': true, active: tab === 1 })} href="#" onClick={e => setTab(1)}>Description</a>
+                </li>
+                <li className="nav-item">
+                  <a className={classNames({ 'nav-link': true, active: tab === 2 })} href="#" onClick={e => setTab(2)}>Specification</a>
+                </li>
+                <li className="nav-item">
+                  <a className={classNames({ 'nav-link': true, active: tab === 3 })} href='#' onClick={e => setTab(3)}>Reviews</a>
+                </li>
+              </ul>
+              <br />
+              {renderTabPane(product)}
             </div>
           </div>
         </div>
